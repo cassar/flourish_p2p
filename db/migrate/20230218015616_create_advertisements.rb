2eebdfd6ab3989d27c3001746f9ae4fbff332e48
Luke Cassar <luke.cassar@uqconnect.edu.au>
2023-02-18 11:56:57 +1000

    Add advertisement model

---
+++ db/migrate/20230218015616_create_advertisements.rb
@@ -0,0 +1,10 @@
+class CreateAdvertisements < ActiveRecord::Migration[7.0]
+  def change
+    create_table :advertisements do |t|
+      t.integer :cents
+      t.string :currency
+
+      t.timestamps
+    end
+  end
+end
