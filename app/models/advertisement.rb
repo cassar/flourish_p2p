cd7e92a4c532bbc8b5a5566560681977596676a3
Luke Cassar <luke.cassar@uqconnect.edu.au>
2023-02-18 13:42:23 +1000

    Add formatting for currency amounts

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,2 +1,7 @@
 class Advertisement < ApplicationRecord
+  validates :cents, :currency, presence: true
+
+  def amount
+    Money.from_cents(cents, currency).format
+  end
 end
--- test/models/advertisement_test.rb
+++ test/models/advertisement_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class AdvertisementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires a cents" do
+    error = assert_raises ActiveRecord::RecordInvalid do
+      Advertisement.new.save!
+    end
+    assert_match /Cents can't be blank/, error.message
+  end
+
+  test "requires a currency" do
+    error = assert_raises ActiveRecord::RecordInvalid do
+      Advertisement.new.save!
+    end
+    assert_match /Currency can't be blank/, error.message
+  end
+
+  test "amount returns proper format" do
+    assert_equal "$100.00", advertisements(:one).amount
+  end
 end
